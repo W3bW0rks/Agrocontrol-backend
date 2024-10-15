b29799b138d1e9feb6eb2034f6ec38ebb03942b2
JP19-03 <u202014511@upc.edu.pe>
2024-10-15 12:13:18 -0500

    chore: project setup.

---
+++ src/main/java/com/agrocontrol/backend/AgroControlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agrocontrol.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroControlBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroControlBackendApplication.class, args);
+    }
+
+}
