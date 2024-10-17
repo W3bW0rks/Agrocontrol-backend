f3333f6da9641d6fb86131a0ef10a93e963ce581
anonimat0 <u202114714@upc.edu.pe>
2024-10-17 11:08:03 -0500

    feat: repositores in infrastructure of profiles added

---
+++ src/main/java/com/agrocontrol/backend/profiles/infrastructure/persistence/jpa/repositories/DistributorRepository.java
@@ -0,0 +1,13 @@
+package com.agrocontrol.backend.profiles.infrastructure.persistence.jpa.repositories;
+
+
+import com.agrocontrol.backend.profiles.domain.model.aggregates.Distributor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DistributorRepository extends JpaRepository<Distributor, Long> {
+    Optional<Distributor> findDistributorByUserId(Long id);
+}
