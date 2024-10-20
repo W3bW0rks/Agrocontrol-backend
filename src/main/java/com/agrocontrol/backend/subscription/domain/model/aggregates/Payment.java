5748490b7e82cbe2edea8aa27db3d2c896193ce1
JP19-03 <u202014511@upc.edu.pe>
2024-10-20 11:55:22 -0500

    feat(payments): add payment aggregate.

---
+++ src/main/java/com/agrocontrol/backend/subscription/domain/model/aggregates/Payment.java
@@ -0,0 +1,52 @@
+package com.agrocontrol.backend.subscription.domain.model.aggregates;
+
+
+import com.agrocontrol.backend.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.agrocontrol.backend.subscription.domain.model.commands.CreatePaymentCommand;
+import com.agrocontrol.backend.subscription.domain.model.commands.RenewPaymentCommand;
+import com.agrocontrol.backend.subscription.domain.model.commands.UpdatePlanTypeCommand;
+import com.agrocontrol.backend.subscription.domain.model.valueobjects.PlanTypes;
+import com.agrocontrol.backend.subscription.domain.model.valueobjects.UserId;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Entity
+public class Payment extends AuditableAbstractAggregateRoot<Payment> {
+
+    private PlanTypes planType;
+
+    @Embedded
+    private UserId userId;
+
+    private LocalDate startDate;
+
+    private LocalDate renewalDate;
+
+    private String status;
+
+    private double cost;
+
+    protected Payment() {}
+
+    public Payment(CreatePaymentCommand command) {
+        this.planType = command.planType();
+        this.userId = command.userId();
+        this.startDate = command.startDate();
+        this.renewalDate = command.renewalDate();
+        this.status = command.status();
+        this.cost = command.cost();
+    }
+
+    public void updatePlanType(UpdatePlanTypeCommand command) {
+        this.planType = command.planType();
+    }
+
+    public void renewPlan(RenewPaymentCommand command) {
+        this.renewalDate = command.renewalDate();
+    }
+}
