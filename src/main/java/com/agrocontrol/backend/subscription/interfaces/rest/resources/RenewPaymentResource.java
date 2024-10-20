f447088b8fc376f117921ddb0e2d890f59b60155
JP19-03 <u202014511@upc.edu.pe>
2024-10-20 12:59:50 -0500

    refactor(payments): remove payment id in resources.

--- src/main/java/com/agrocontrol/backend/subscription/interfaces/rest/resources/RenewPaymentResource.java
+++ src/main/java/com/agrocontrol/backend/subscription/interfaces/rest/resources/RenewPaymentResource.java
@@ -3,7 +3,6 @@ package com.agrocontrol.backend.subscription.interfaces.rest.resources;
 import java.time.LocalDate;
 
 public record RenewPaymentResource(
-        LocalDate renewalDate,
-        Long paymentId
+        LocalDate renewalDate
 ) {
 }
--- src/main/java/com/agrocontrol/backend/subscription/interfaces/rest/resources/UpdatePlantTypeResource.java
+++ src/main/java/com/agrocontrol/backend/subscription/interfaces/rest/resources/UpdatePlantTypeResource.java
@@ -3,7 +3,6 @@ package com.agrocontrol.backend.subscription.interfaces.rest.resources;
 import com.agrocontrol.backend.subscription.domain.model.valueobjects.PlanTypes;
 
 public record UpdatePlantTypeResource(
-        PlanTypes planType,
-        Long paymentId
+        PlanTypes planType
 ) {
 }
