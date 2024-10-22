a0415cc873ba3fcde1c6be5fbaba51a92531ce5c
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-21 21:30:22 -0500

    feat(store): add command and query service contracts.

---
+++ src/main/java/com/agrocontrol/backend/store/domain/services/ProductQueryService.java
@@ -0,0 +1,15 @@
+﻿package com.agrocontrol.backend.store.domain.services;
+
+import com.agrocontrol.backend.store.domain.model.aggregates.Product;
+import com.agrocontrol.backend.store.domain.model.queries.GetProductByIdQuery;
+import com.agrocontrol.backend.store.domain.model.queries.GetProductByNameQuery;
+import com.agrocontrol.backend.store.domain.model.queries.GetProductByUserIdQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductQueryService {
+    List<Product> handle(GetProductByUserIdQuery query);
+    Optional<Product> handle(GetProductByIdQuery query);
+    Optional<Product> handle(GetProductByNameQuery query);
+}
