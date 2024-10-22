bd4512ec0853b5f4410577d61b00e986227d7d47
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-21 21:51:45 -0500

    feat(store): add product command and query service implementations.

---
+++ src/main/java/com/agrocontrol/backend/store/application/internal/queryservices/ProductQueryServiceImpl.java
@@ -0,0 +1,37 @@
+﻿package com.agrocontrol.backend.store.application.internal.queryservices;
+
+import com.agrocontrol.backend.store.domain.model.aggregates.Product;
+import com.agrocontrol.backend.store.domain.model.queries.GetProductByIdQuery;
+import com.agrocontrol.backend.store.domain.model.queries.GetProductByNameQuery;
+import com.agrocontrol.backend.store.domain.model.queries.GetProductByUserIdQuery;
+import com.agrocontrol.backend.store.domain.services.ProductQueryService;
+import com.agrocontrol.backend.store.infrastructure.persistence.jpa.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductQueryServiceImpl implements ProductQueryService {
+
+    private final ProductRepository productRepository;
+
+    public ProductQueryServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> handle(GetProductByUserIdQuery query) {
+        return productRepository.findByUserId(query.userId());
+    }
+
+    @Override
+    public Optional<Product> handle(GetProductByIdQuery query) {
+        return productRepository.findById(query.productId());
+    }
+
+    @Override
+    public Optional<Product> handle(GetProductByNameQuery query) {
+        return productRepository.findByName(query.name());
+    }
+}
