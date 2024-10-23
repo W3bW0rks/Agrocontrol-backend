ca29d3380e03bde4f41b355942593168c38adc4d
Alejandro Espino <espinofloresa@gmail.com>
2024-10-22 17:40:38 -0700

    feat(field): add CreateFieldCommand.

---
+++ src/main/java/com/agrocontrol/backend/fields/domain/commands/CreateFieldCommand.java
@@ -0,0 +1,37 @@
+package com.agrocontrol.backend.fields.domain.commands;
+
+import com.agrocontrol.backend.fields.domain.valueobjects.ProducerId;
+
+/**
+ *  command to create a Field
+ * @param producerId the producer ID
+ * @param fieldName the field name
+ * @param location the location
+ * @param size the size
+ */
+public record CreateFieldCommand(
+        ProducerId producerId,
+        String fieldName,
+        String location,
+        Integer size
+) {
+    /**
+     * Constructor
+     * @param producerId the producer ID
+     *                   cannot be null or empty
+     * @param fieldName the field name
+     *                  cannot be null or empty
+     * @param location the location
+     *                 cannot be null or empty
+     * @param size the size
+     *             cannot be null or less than zero
+     */
+    public CreateFieldCommand {
+        if(fieldName == null || fieldName.isBlank())
+            throw new IllegalArgumentException("fieldName cannot be null or empty");
+        if(location== null || location.isBlank())
+            throw new IllegalArgumentException("location cannot be null or empty");
+        if(size == null || size <= 0)
+            throw new IllegalArgumentException("size must be greater than 0");
+    }
+}
