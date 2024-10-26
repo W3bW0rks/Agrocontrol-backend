3496ff6d5772d912afd3a2393a05e2d307dd26e9
Alejandro Espino <espinofloresa@gmail.com>
2024-10-25 20:55:01 -0700

    fix(field): remove GetAllFieldsQuery.

--- src/main/java/com/agrocontrol/backend/fields/domain/model/queries/GetAllFieldsQuery.java
+++
@@ -1,8 +0,0 @@
-package com.agrocontrol.backend.fields.domain.model.queries;
-
-/**
- * Query to retrieve all fields.
- * This query does not apply any filters or parameters.
- */
-public record GetAllFieldsQuery() {
-}
