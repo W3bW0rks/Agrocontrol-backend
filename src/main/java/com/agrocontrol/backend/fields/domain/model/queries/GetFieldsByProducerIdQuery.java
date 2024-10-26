a7d629331166884be579039a9f3538571676c96c
Alejandro Espino <espinofloresa@gmail.com>
2024-10-25 20:37:34 -0700

    fix(field): fixed GetFieldsByProducerIdQuery query.

--- src/main/java/com/agrocontrol/backend/fields/domain/model/queries/GetFieldsByProducerIdQuery.java
+++ src/main/java/com/agrocontrol/backend/fields/domain/model/queries/GetFieldsByProducerIdQuery.java
@@ -1,10 +1,9 @@
 package com.agrocontrol.backend.fields.domain.model.queries;
 
-import com.agrocontrol.backend.fields.domain.model.valueobjects.ProducerId;
 
 /**
  *  Query to get field by a producer ID
  * @param producerId
  */
-public record GetFieldsByProducerIdQuery(ProducerId producerId) {
+public record GetFieldsByProducerIdQuery(Long producerId) {
 }
