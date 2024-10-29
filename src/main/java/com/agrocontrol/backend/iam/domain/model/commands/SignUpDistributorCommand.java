18b68a77224f4a5fa834da787150f3473c9dc54c
GonzaQuedenaUPC <u202113656@upc.edu.pe>
2024-10-28 22:27:34 -0500

    fix(iam): add exceptions in commands for sign up of distributors and producers.

--- src/main/java/com/agrocontrol/backend/iam/domain/model/commands/SignUpDistributorCommand.java
+++ src/main/java/com/agrocontrol/backend/iam/domain/model/commands/SignUpDistributorCommand.java
@@ -2,4 +2,12 @@ package com.agrocontrol.backend.iam.domain.model.commands;
 
 public record SignUpDistributorCommand(String fullName, String email, String password,
                                        String city, String country, String phone, String companyName, String ruc) {
+    public SignUpDistributorCommand {
+        if (!email.contains("@"))
+            throw new RuntimeException("Invalid email");
+        if (ruc.length() != 11)
+            throw new RuntimeException("Invalid RUC");
+        if (phone.length() != 9)
+            throw new RuntimeException("Invalid phone number");
+    }
 }
